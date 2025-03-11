3afbe5d5bd12373b2e312c5c78ef571b9991b978
limkyulee <mud0308@gmail.com>
2025-03-11 13:17:39 +0900

    Enum clear

--- src/enumeration/example1/DiscountService.java
+++ src/enumeration/example1/DiscountService.java
@@ -2,8 +2,9 @@ package enumeration.example1;
 
 public class DiscountService {
 
-//  주석을 달아놓느다고 하더라도 String 타입을 받도록 하게되면
-//  StringGrade 의 값이 아닌 다른 값을 받아도 문제가 되지않기 떄문에 오류를 막을 수 없다.
+//  주석을 작성해 놓더라도 파라미터를 String 타입으로 받게되면
+//  StringGrade 의 제한된 값이 아닌 다른 값을 받아도 문제가 되지 않기 떄문에 오류를 막을 수 없다.
+
 //  FIXME : StringGrade 를 사용하세요.
     public int discount(String grade, int price) {
         int discountPercent = 0;
