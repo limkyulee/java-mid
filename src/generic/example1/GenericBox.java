bdc91369f0401bf93dca7899cb049f31b40aceed
limkyulee <mud0308@gmail.com>
2025-03-18 16:35:40 +0900

    generic

--- src/generic/example1/GenericBox.java
+++ src/generic/example1/GenericBox.java
@@ -3,7 +3,8 @@ package generic.example1;
 //  FIXME : Generic Class
 /**
  *
- * T 에는 기본형을 제외한 원하는 타입을 모두 지정할 수 있다. (객체 참조형만 가능)
+ * T 에는 기본형을 제외한 원하는 타입을 모두 지정할 수 있다.
+ * Wrapper 타입을 사용하면 됨.
  * @param <T> 타입 매개변수
  */
 public class GenericBox<T> {
---
+++ src/generic/example1/RowTypeMain.java
@@ -0,0 +1,13 @@
+package generic.example1;
+
+public class RowTypeMain {
+    public static void main(String[] args) {
+//        GenericBox<Object> objectGenericBox = new GenericBox<>(); // 권장 포멧
+
+//      FIXME : Generic type 사용 시, 주의할 점.
+//        > 아래와 같이 "<>" 를 지정하지 않을 수 있는데
+        GenericBox integer = new GenericBox();
+        integer.set(10);
+        Integer result = (Integer) integer.get();
+    }
+}
